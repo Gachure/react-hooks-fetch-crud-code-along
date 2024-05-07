82f8f2f1f17cd4b82e46dc1f1a2c8b52deea052e
victor <victor.gachure@student.moringaschool>
2024-05-07 10:21:49 +0300

    done lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useState, useEffect } from 'react';
+import ItemForm from './ItemForm';
+import Filter from './Filter';
+import Item from './Item';
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([]); // This state setter should be used to update the items
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((data) => setItems(data)); // Ensure you're updating the state with fetched data
+  }, []);
 
   function handleCategoryChange(category) {
-    setSelectedCategory(category);
+    setSelectedCategory(category); // This setter is used to update the category state
+  }
+
+  // You should add the callback for updating items when creating a new item
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]); // This should add a new item to the existing state
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,11 +30,8 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
+      <ItemForm onAddItem={handleAddItem} /> 
+      <Filter category={selectedCategory} onCategoryChange={handleCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} item={item} />
